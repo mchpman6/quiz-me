6021f779da05e63fd36e2c9e1af62273316ce778
Ellen Chapman <mchpman6@memphis.edu>
2019-12-07 23:38:09 -0600

    demo 10 complete

--- app/controllers/mc_questions_controller.rb
+++ app/controllers/mc_questions_controller.rb
@@ -34,4 +34,38 @@ class McQuestionsController < ApplicationController
       }
     end
   end
+
+  def edit
+    question = McQuestion.find(params[:id])
+    respond_to do |format|
+      format.html { render :edit, locals: { question: question } }
+    end
+  end
+
+  def update
+    question = McQuestion.find(params[:id])
+    respond_to do |format|
+      format.html {
+        if question.update(params.require(:mc_question).permit(:question, :answer, :distractor_1, :distractor_2))
+          flash[:success] = 'Question updated successfully'
+          redirect_to mc_questions_url
+        else
+          flash.now[:error] = 'Error: Question could not be updated'
+          render :edit, locals: { question: question }
+        end
+      }
+    end
+  end
+
+  def destroy
+    question = McQuestion.find(params[:id])
+    question.destroy
+    respond_to do |format|
+      format.html {
+        flash[:success] = 'Questions removed successfully'
+        redirect_to mc_questions_url
+      }
+    end
+  end
+
 end
